1d86bdfb9d792b4cde55a77409d738ad24195b65
浦同矫 <15391493308@163.com>
2022-07-10 23:09:21 +0800

    Redis集成菜单功能

--- yeb-server/src/main/java/cn/ut/server/service/impl/MenuServiceImpl.java
+++ yeb-server/src/main/java/cn/ut/server/service/impl/MenuServiceImpl.java
@@ -6,8 +6,11 @@ import cn.ut.server.pojo.Menu;
 import cn.ut.server.service.IMenuService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -24,12 +27,23 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IM
 
     @Autowired
     private MenuMapper menuMapper;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
     /**
      * 通过用户id查询菜单列表
      * @return
      */
     @Override
     public List<Menu> getMenusByAdminId() {
-        return menuMapper.getMenusByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        Integer adminId = ((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        //从redis获取菜单数据 如果为空 去数据库获取
+        List<Menu> menus = (List<Menu>) valueOperations.get("menu_" + adminId);
+        if (CollectionUtils.isEmpty(menus)){
+            menus = menuMapper.getMenusByAdminId(adminId);
+            //将数据设置到redis中
+            valueOperations.set("menu_" + adminId, menus);
+        }
+        return menus;
     }
 }
